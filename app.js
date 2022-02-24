9faca5a1c42801635dc490a6a6c4bb30c03c6c67
Obonye <66769791+Obonye@users.noreply.github.com>
2022-02-25 00:29:33 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const getAdvice = async () => {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+  document.getElementById("adviceID").innerHTML = `Advice # ${data.slip.id}`;
+  document.getElementById("advice").innerHTML = ` ${data.slip.advice}`;
+
+  console.log(data);
+};
+getAdvice();
